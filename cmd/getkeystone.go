40e8552de02bf2fbc118990684970e8edca666bf
Trilok Khairnar <trilok@platform9.com>
2024-07-01 21:14:08 +0530

    do ont fetch keystone pwd in cmd/getkeystone.go

--- cmd/getkeystone.go
+++ cmd/getkeystone.go
@@ -4,7 +4,6 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"go.uber.org/zap"
 )
 
 func init() {
@@ -18,17 +17,6 @@ var getKeystoneCmd = &cobra.Command{
 	Short: "Get Keystone related attributes",
 	Long:  `Get Keystone related attributes`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		cfgMgr := GetCfg()
-		zap.L().Debug("Get keystone password")
-		_, err := cfgMgr.GetKeystonePassword(serviceName)
-		if err != nil {
-			zap.L().Info("Error getting keystone password")
-			return err
-		}
-		// Do not print password
-		// It causes a security alert
-		// Clear-text logging of sensitive information
-		// fmt.Println(password)
 		return nil
 	},
 }
